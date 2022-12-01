f0d681c2c16adf5bb5fe9c9bd94dc37e80f92504
romi <romain.gourmelen@gmail.com>
2022-12-01 21:55:54 +0100

    renommage day1

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -5,20 +5,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class AdventOfCode {
+public class Day1 {
 
     public static void main(String[] args) throws IOException {
 
         List<String> inputs = FileReader.read(1);
 
-        System.out.println(traitement(inputs));
+        System.out.println(reponse1(inputs));
+        System.out.println(reponse2(inputs));
     }
 
-    private static String traitement(List<String> inputs) {
+    private static int reponse1(List<String> inputs) {
 
-        int sumGlobal = 0;
-        int maxValue = 0;
-        int currentValue = 0;
+        List<Integer> values = new ArrayList<>();
+        values.add(0);
+        for (String input: inputs) {
+            if ("".equals(input)) {
+                values.add(0);
+            } else {
+                values.set(values.size() - 1, values.get(values.size() - 1) + Integer.parseInt(input));
+            }
+        }
+
+        return Collections.max(values);
+    }
+
+    private static int reponse2(List<String> inputs) {
+
         List<Integer> values = new ArrayList<>();
         values.add(0);
         for (String input: inputs) {
@@ -38,6 +51,6 @@ public class AdventOfCode {
         Integer max3 = Collections.max(values);
         values.remove(max3);
 
-        return "" + (max1 + max2 + max3);
+        return (max1 + max2 + max3);
     }
 }
