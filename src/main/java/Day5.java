61466a086baa483c54c2cbe9dafa43be4a6dcc66
romi <romain.gourmelen@gmail.com>
2022-12-04 12:36:04 +0100

    préparation day5

---
+++ src/main/java/Day5.java
@@ -0,0 +1,32 @@
+import util.FileReader;
+
+import java.util.List;
+
+public class Day5 {
+
+    public static void main(String[] args) throws Exception {
+
+        List<String> inputs = FileReader.read(5);
+
+        System.out.println(reponse1(inputs));
+//        System.out.println(reponse2(inputs));
+    }
+
+    private static int reponse1(List<String> inputs) {
+        int result = 0;
+        for (String input: inputs.stream().filter(s -> !"".equals(s)).toList()) {
+
+        }
+
+        return result;
+    }
+
+    private static int reponse2(List<String> inputs) {
+        int result = 0;
+        for (String input: inputs.stream().filter(s -> !"".equals(s)).toList()) {
+
+        }
+
+        return result;
+    }
+}
