7454b7db03084108d7467e217ed26ddbf4c8dc86
romi <romain.gourmelen@gmail.com>
2022-12-04 12:35:15 +0100

    Day 4 - part1 & part2

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -13,13 +13,23 @@ public class Day4 {
         List<String> inputs = FileReader.read(4);
 
         System.out.println(reponse1(inputs));
-//        System.out.println(reponse2(inputs));
+        System.out.println(reponse2(inputs));
     }
 
     private static int reponse1(List<String> inputs) {
         int result = 0;
         for (String input: inputs.stream().filter(s -> !"".equals(s)).toList()) {
+            String[] pairs = input.split(",");
 
+            String[] pair1 = pairs[0].split("-");
+            String[] pair2 = pairs[1].split("-");
+
+            if ((Integer.parseInt(pair1[0]) <= Integer.parseInt(pair2[0]) &&
+                    Integer.parseInt(pair1[1]) >= Integer.parseInt(pair2[1])) ||
+                (Integer.parseInt(pair1[0]) >= Integer.parseInt(pair2[0]) &&
+                        Integer.parseInt(pair1[1]) <= Integer.parseInt(pair2[1]))) {
+                result += 1;
+            }
         }
 
         return result;
@@ -27,8 +37,18 @@ public class Day4 {
 
     private static int reponse2(List<String> inputs) {
         int result = 0;
-        for (int i = 0; i < inputs.size(); i = i + 3) {
+        for (String input: inputs.stream().filter(s -> !"".equals(s)).toList()) {
+            String[] pairs = input.split(",");
 
+            String[] pair1 = pairs[0].split("-");
+            String[] pair2 = pairs[1].split("-");
+
+            if ((Integer.parseInt(pair1[0]) <= Integer.parseInt(pair2[0]) &&
+                    Integer.parseInt(pair1[1]) >= Integer.parseInt(pair2[0])) ||
+                    (Integer.parseInt(pair1[0]) >= Integer.parseInt(pair2[0]) &&
+                            Integer.parseInt(pair1[0]) <= Integer.parseInt(pair2[1]))) {
+                result += 1;
+            }
         }
 
         return result;
