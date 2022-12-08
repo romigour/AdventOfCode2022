58a6727ba6a10f8730d49cbdc68aa9ec227cc2e4
romi <romain.gourmelen@gmail.com>
2022-12-08 23:02:42 +0100

    Day 8 - part 1 & 2 (ugly code) preparation day7

--- src/main/java/Day8.java
+++ src/main/java/Day8.java
@@ -12,14 +12,83 @@ public class Day8 {
         List<String> inputs = FileReader.read(8);
 
         System.out.println(reponse1(inputs));
-//        System.out.println(reponse2(inputs));
+        System.out.println(reponse2(inputs));
     }
 
     private static int reponse1(List<String> inputs) {
         int result = 0;
+        int[][] jungle = new int[inputs.size()][inputs.get(0).length()];
 
-        for (String input: inputs) {
+        for (int y = 0; y < inputs.size(); y++) {
+            String[] inputArray = inputs.get(y).split("");
+            for (int x = 0; x < inputArray.length; x++) {
+                jungle[y][x] = Integer.parseInt(inputArray[x]);
+            }
+        }
 
+
+        for (int y = 0; y < jungle.length; y++) {
+            for (int x = 0; x < jungle[y].length; x++) {
+
+                int arbre = jungle[y][x];
+
+                boolean isVisible = true;
+
+                // Gauche
+                int a = x - 1;
+                int b = y;
+                while (a >= 0 && isVisible) {
+                    if (arbre <= jungle[b][a])
+                        isVisible = false;
+                    a--;
+                }
+                if (isVisible) {
+                    result++;
+                    continue;
+                }
+
+                // Droite
+                a = x + 1;
+                b = y;
+                isVisible = true;
+                while (a < jungle[y].length && isVisible) {
+                    if (arbre <= jungle[b][a])
+                        isVisible = false;
+                    a++;
+                }
+                if (isVisible) {
+                    result++;
+                    continue;
+                }
+
+
+                // Haut
+                a = x;
+                b = y - 1;
+                isVisible = true;
+                while (b >= 0 && isVisible) {
+                    if (arbre <= jungle[b][a])
+                        isVisible = false;
+                    b--;
+                }
+                if (isVisible) {
+                    result++;
+                    continue;
+                }
+
+                // Bas
+                a = x;
+                b = y + 1;
+                isVisible = true;
+                while (b < jungle.length && isVisible) {
+                    if (arbre <= jungle[b][a])
+                        isVisible = false;
+                    b++;
+                }
+                if (isVisible) {
+                    result++;
+                }
+            }
         }
 
         return result;
@@ -28,9 +97,75 @@ public class Day8 {
     private static int reponse2(List<String> inputs) {
 
         int result = 0;
+        int[][] jungle = new int[inputs.size()][inputs.get(0).length()];
 
-        for (String input: inputs) {
+        for (int y = 0; y < inputs.size(); y++) {
+            String[] inputArray = inputs.get(y).split("");
+            for (int x = 0; x < inputArray.length; x++) {
+                jungle[y][x] = Integer.parseInt(inputArray[x]);
+            }
+        }
 
+        for (int y = 0; y < jungle.length; y++) {
+            for (int x = 0; x < jungle[y].length; x++) {
+
+                int arbre = jungle[y][x];
+
+                int gauche = 0;
+                int droite = 0;
+                int haut = 0;
+                int bas = 0;
+
+                boolean isVisible = true;
+
+                // Gauche
+                int a = x - 1;
+                int b = y;
+                while (a >= 0 && isVisible) {
+                    if (arbre <= jungle[b][a])
+                        isVisible = false;
+                    a--;
+                    gauche++;
+                }
+
+                // Droite
+                a = x + 1;
+                b = y;
+                isVisible = true;
+                while (a < jungle[y].length && isVisible) {
+                    if (arbre <= jungle[b][a])
+                        isVisible = false;
+                    a++;
+                    droite++;
+                }
+
+                // Haut
+                a = x;
+                b = y - 1;
+                isVisible = true;
+                while (b >= 0 && isVisible) {
+                    if (arbre <= jungle[b][a])
+                        isVisible = false;
+                    b--;
+                    haut++;
+                }
+
+                // Bas
+                a = x;
+                b = y + 1;
+                isVisible = true;
+                while (b < jungle.length && isVisible) {
+                    if (arbre <= jungle[b][a])
+                        isVisible = false;
+                    b++;
+                    bas++;
+                }
+
+                int val = gauche * droite * haut * bas;
+                if (val > result) {
+                    result = val;
+                }
+            }
         }
 
         return result;
