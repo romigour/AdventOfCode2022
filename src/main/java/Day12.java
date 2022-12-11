be51c2667415a57b3ea2e532847dfef7416e335e
romi <romain.gourmelen@gmail.com>
2022-12-11 18:58:30 +0100

    Day 11 - part 2 (utilisation du lcm => idée pas de moi -> no scoring)

---
+++ src/main/java/Day12.java
@@ -0,0 +1,29 @@
+import util.FileReader;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Day12 {
+    static long result = 0;
+
+    public static void main(String[] args) throws Exception {
+
+        List<String> inputs = FileReader.read(12);
+
+        System.out.println(reponse1(inputs));
+//        System.out.println(reponse2(inputs));
+    }
+
+    private static long reponse1(List<String> inputs) {
+
+        return result;
+    }
+
+    private static long reponse2(List<String> inputs) {
+
+
+        return 0;
+    }
+
+
+}
